3a99c823d8d9eedc981668818143e2b19893a404
treelite <c.xinle@gmail.com>
2015-11-28 21:22:20 +0800

    Del unused files

--- example/single/src/a/base.js
+++
@@ -1,5 +0,0 @@
-define(function (require, exports, module) {
-    exports.g = function (id) {
-        return document.getElementById(id);
-    };
-});
--- example/single/src/b/base.js
+++
@@ -1,7 +0,0 @@
-define(function (require, exports, module) {
-    var a = require('base');
-
-    exports.sayHello = function (id, name) {
-        a.g(id).innerHTML = 'hello ' + name;
-    };
-});
